dc27af428a64c330411793bba1d91f3e0acdc74f
Windjy E. Jean <Yuugami@users.noreply.github.com>
2024-01-04 16:44:51 -0500

    Adding menu. Will reverse order for list elements.

--- js/script.js
+++ js/script.js
@@ -12,5 +12,34 @@ function carousel() {
         myIndex = 1
     }
     x[myIndex - 1].style.display = "block";
-    setTimeout(carousel, 5000); // Change image every 2 seconds
+    setTimeout(carousel, 5000); // Change image every 5 seconds
+}
+
+function plusDivs(n) {
+    showDivs(myIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {
+        myIndex = 1
+    }
+    if (n < 1) {
+        myIndex = x.length
+    }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[myIndex - 1].style.display = "block";
+}
+
+function toggleMenu() {
+    var x = document.getElementById("theMenu");
+    console.log(x.style.display);
+
+    if (x.style.display == "flex")
+        x.style.display = "none";
+    else
+        x.style.display = "flex";
 }
